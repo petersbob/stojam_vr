85196fbf449f597775489ec7f68025bdca4d1718
Joseph <josephpe45@gmail.com>
2016-11-13 00:20:23 -0600

    send movement info to other clients. very basic.

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-var app = require('express')();
+var express = require('express');
+
+var app = express();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+//var clients = [];
+var clientMap = {};
+/*
+loop through array
+if(clientMap[client.id] == undefined){
+clientMap[client.id] = true;
+create new cube 
+}
+ */
+
+function createClient(new_socket, new_msg) {
+    clients.push({
+        coords: new_msg,
+        id: new_socket.id
+    })
+}
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/lib/three.min.js', function(req, res) {
-    res.sendFile(__dirname + '/lib/three.min.js');
-});
-
-app.get('/assets/*.png', function(req, res) {
-    console.log('found a png asset');
-    res.sendFile(__dirname + '/assets/dirt.png');  // expand to include all images
-});
+app.use('/public',express.static('public'));
 
 io.on('connection', function(socket) {
-    console.log('a user connected');
+    console.log('a user connected. Socket: ' + socket.id);
+
+    socket.emit('socket_id', socket.id);
+    socket.on('update', function(msg) {
+        clientMap[msg.id] = msg;
+        io.emit('update', JSON.stringify(clientMap));
+    });
+    // socket.on('render', function(msg) {
+    //     console.log(msg.x);
+    // });
+    // socket.on('cubeUpdate', msg) {
+    //     clientMap[msg.id].x = msg.x
+    // });
+    socket.on('disconnect', function(socket) {
+        console.log('a user disconnected');
+        delete clientMap[socket.id];
+        io.emit('update', JSON.stringify(clientMap));
+    });
 });
+
+
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
